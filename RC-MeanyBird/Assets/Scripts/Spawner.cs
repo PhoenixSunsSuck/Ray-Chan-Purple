2324edac1fe5ce9ea8d23807f5954af73bac8474
hahahahahahahahahahahahahahahahahahahahahahahahaha <fariskhurram10@gmail.com>
2022-12-08 19:58:33 -0500

    RC-MeanyBird

--- RC-MeanyBird/Assets/Scripts/Spawner.cs
+++ RC-MeanyBird/Assets/Scripts/Spawner.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    //Object that we will attach the script for spawing object
+    [Header("Spikes Object for controlling the game")]
+    public GameObject spikes;
+    //Height of position of the spikes
+    [Header("Default Height")]
+    public float height;
     // Start is called before the first frame update
     void Start()
     {
-
+        //Start function repeating every 4 seconds
+        InvokeRepeating("InstantObjects", 1f, 4f);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //Position for the gameobjects
+        transform.position = new Vector3(5, Random.Range(-height, height), 0);
+    }
+    //InstantiateObjects.Function
+    void InstantObjects()
+    {
+        //Spawn object by postion and rotation
+        Instantiate(spikes, transform.position, transform.rotation);
     }
 }
