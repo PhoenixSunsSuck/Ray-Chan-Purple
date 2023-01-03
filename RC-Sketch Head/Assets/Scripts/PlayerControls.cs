8c2952a02b0e71ad1c02413d41e52403a286e532
rebecca <102189845+hellomynameisaj@users.noreply.github.com>
2023-01-03 16:58:10 -0500

    RC-SketchHead
    Step 41

--- RC-Sketch Head/Assets/Scripts/PlayerControls.cs
+++ RC-Sketch Head/Assets/Scripts/PlayerControls.cs
@@ -5,6 +5,11 @@ using UnityEngine.UI;
 
 public class PlayerControls : MonoBehaviour
 {
+    //Font Text
+    [Header("Score Text")]
+    public Text scoreText;
+    //score of game
+    private float topScore = 0.0f;
     //RigidBody2D object that is stored
     [Header("Rigidbody")]
     public Rigidbody2D rb;
@@ -27,6 +32,16 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if players velocity is greater than 0
+        //and position on the y axis is greater
+        //than the score
+        if(rb.velocity.y > 0 && transform.position.y > topScore)
+        {
+            //Score equals players position
+            topScore = transform.position.y;
+        }
+        //Text for the score equals to the top score
+        scoreText.text = "Score:" + Mathf.Round(topScore).ToString();
         //movement equals Horizontal movement
         //multiplied by the movement speed 
         movement = Input.GetAxis("Horizontal") * movementSpeed;
@@ -36,10 +51,11 @@ public class PlayerControls : MonoBehaviour
             //object faces to the left
             this.GetComponent<SpriteRenderer>().flipX = false;
         }
-        
+        //object faces to the right
+        this.GetComponent<SpriteRenderer>().flipX = true;
     }
     //fixedupdate called every fixed frame-rate frame.
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         //vector2 which is (x,y) velocity
         //equals to the velocity of the Rigidbody2d
