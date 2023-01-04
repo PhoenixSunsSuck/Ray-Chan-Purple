65cf81664d5eb4614654faf41d2a74d1fa81224a
rebecca <102189845+hellomynameisaj@users.noreply.github.com>
2023-01-04 16:56:24 -0500

    RC-SketchHead

--- RC-Sketch Head/Assets/Scripts/GameController.cs
+++ RC-Sketch Head/Assets/Scripts/GameController.cs
@@ -17,15 +17,30 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         //Integer i equals 1000
         for (int i = 0; i < 1000; i++)
         {
             //Execute SpawnPlatforms
             SpawnPlatforms();
         }
+        //Integer i equals 1000
+        for (int i = 0; i < 1000; i++)
+        {
+            //Execute SpawnPlatforms
+            SpawnFakePlatforms();
+        }
     }
     void SpawnPlatforms()
     {
+        //Spawn platforms randomly on the x axi and place them on the Y axis every 2.5
+        Instantiate(platform, new Vector3(Random.value * 10 - 5f, pos, 0.5f), Quaternion.identity);
+        pos += 2.5f;
+    }
+
+    void SpawnFakePlatforms()
+    {
+        
         //Spawn platforms randomly on the x axi and place them on the Y axis every 2.5
         Instantiate(platform, new Vector3(Random.value * 10 - 5f, pos, 0.5f), Quaternion.identity);
         pos += 2.5f;
