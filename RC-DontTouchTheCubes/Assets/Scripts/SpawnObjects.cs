00cc5eb942ca6c4ae98e2cca0ef83af92b87963d
Alison <bunbunsoswag@gmail.com>
2023-01-10 17:13:34 -0500

    RC-DontTouchTheCubes
    Step 18

---
+++ RC-DontTouchTheCubes/Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    [Header("Spawn Cube Object")]
+    //Cube that is going to be spawned
+    public GameObject spawnCube;
+    //difficulty of the game
+    [Header("Default Difficulaty")]
+    public float difficulty = 40f;
+    //Time for the next cube
+    //to be spawned.
+    float spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
